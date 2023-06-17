26320cfc10a9fad47672f1eee4498a51a5a801ea
henriz <henri.zavareze@gmail.com>
2023-06-17 15:49:49 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { HostListener, Component, Directive, ElementRef, Input, Renderer2  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -57,5 +57,48 @@ export class AppComponent {
     if (index !== -1) {
       this.tasks.splice(index, 1);
     }
+  }
+  /////
+  loop:any[]= [];
+  type = 0;
+  constructor() {
+  }
+  @HostListener('click', ['$event.target']) onClick(x:any) {
+    console.log('obj', x);
+    // if (this.type == 1) { //loop
+    x.style = "border: 1px dashed green";
+      this.loop.push(x)
+      this.processLoop();
+    // }
+  }
+  processLoop() {
+    if (this.loop.length == 2) {
+      console.log(this.loop);
+      let filterName = '';
+      let filterClass = '';
+      if (this.loop[0].nodeName == this.loop[1].nodeName) {
+        if (this.loop[0].className != '' && this.loop[0].className == this.loop[1].className) {
+          filterClass = '.'+this.loop[0].className.split(" ").join('.');
+        }
+        if (this.loop[0].name && this.loop[0].name == this.loop[1].name) {
+          filterName = '[name=\''+this.loop[0].name+'\']';
+        }
+        const elements = document.querySelectorAll(this.loop[0].nodeName+filterClass+filterName)
+        elements.forEach((el:any) => {
+          el.style = "border: 1px solid red";
+        })
+      } 
+    }
+  }
+  startClickInput() {
+
+  }
+  startInputClick() {
+
+  }
+  startLoopClick() {
+    console.log('starting loop');
+    this.type = 1;
+
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,11 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
